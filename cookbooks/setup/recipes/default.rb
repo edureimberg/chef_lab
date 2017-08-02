3f010384284195528a211cc52e1e8e4dc8d4f17f
Eduardo Reimberg <eduardo.reimberg@vagas.com.br>
2017-08-02 09:32:26 -0300

    Criação dos cookbooks setup e apache

---
+++ cookbooks/setup/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: setup
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
---
+++ cookbooks/setup/recipes/pacotes.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: setup
+# Recipe:: pacotes
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'vim' do
+  action :install
+end

8698e2e33a4c0f5a479014262e7080398069f302
Eduardo Reimberg <eduardo.reimberg@vagas.com.br>
2017-08-02 10:25:12 -0300

    Alteração da recipe apache - include_recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,18 +4,5 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-package 'apache2' do
-  action :install
-end
+include_recipe "apache::server"
 
-file '/var/www/html/index.html' do
-  content '<h1>Página de teste</h1>'
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-service 'apache2' do
-  action [ :enable, :start]
-end
